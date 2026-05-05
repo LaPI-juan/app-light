e5b2fc4568d73c1c348e764ee0acb5652819b3ba
LaPI-juan <juan.saldana2504@gmail.com>
2026-05-05 00:19:58 -0600

    Update inferencia.py

--- inferencia.py
+++ inferencia.py
@@ -103,7 +103,7 @@ def uso_YOLO(ruta_modelo, rutas_PNG):
         V = CargarVolumen_YOLO(ruta_PNG)
         results = model(V[0,:,:,:])
         r = results
-        conf_arr = r.boxes.conf.cpu().numpy()
+        conf_arr = r[0].boxes.conf.cpu().numpy()
 		
         if conf_arr.size and conf_arr.max() > 0.65:
             i += 1
